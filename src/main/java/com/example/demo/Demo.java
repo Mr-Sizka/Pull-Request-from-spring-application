97d452e69514c9bd2f16f5a5a658300b9d746954
Sisuka Udawatta <works.sisuka@gmail.com>
2023-06-15 11:20:57 +0530

    1

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.transport.CredentialsProvider;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.egit.github.core.PullRequestMarker;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.client.GitHubClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.eclipse.egit.github.core.PullRequest;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.service.PullRequestService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import java.io.IOException;
+
 
 @CrossOrigin
 @Controller
@@ -17,36 +22,51 @@ public class Demo {
 
     @GetMapping
     public String get(){
-        return pullReq(
-                "C:\\Users\\sisuk\\Downloads\\demo",
-                "https://github.com/Mr-Sizka/demo.git",
-                "mr-sizka",
-                "Sisuka#9ruuqpjpy"
-        );
+        return pullReq();
     }
 
-    public String pullReq(String dir, String gitDir, String userName, String pass) {
+    public String pullReq() {
+
+        String username = "mr-sizka";
+        String password = "Sisuka#9ruuqpjpy";
+        String repositoryOwner = "Mr-Sizka";
+        String repositoryName = "demo";
+
+        GitHubClient client = new GitHubClient();
+        client.setCredentials(username, password);
+
 
         try {
+            // Create a new pull request
+            PullRequestService pullRequestService = new PullRequestService(client);
+            PullRequest pullRequest = new PullRequest();
+            pullRequest.setTitle("My Pull Request");
+            pullRequest.setBody("This is a pull request from the Java application.");
 
-            Repository localRepo = new FileRepository(dir + "/.git");
             System.out.println(1);
-            Git git = new Git(localRepo);
-            System.out.println(2);
-            CredentialsProvider cp = new UsernamePasswordCredentialsProvider(userName, pass);
-            System.out.println(3);
-            git.pull().setCredentialsProvider(cp).call();
-            System.out.println(4);
-            git.close();
-            System.out.println(5);
-            localRepo.close();
-            System.out.println(6);
+            // Specify the source and destination branches
+            pullRequest.setBase(new PullRequestMarker().setRef("main").setLabel("main"));
+            pullRequest.setHead(new PullRequestMarker().setRef("feet3").setLabel("feet3"));
 
-        } catch (Exception e) {
-            return e.getLocalizedMessage();
+
+            // Get the repository information
+            RepositoryId repositoryId = RepositoryId.create(repositoryOwner, repositoryName);
+            System.out.println(1);
+            RepositoryService repositoryService = new RepositoryService(client);
+            System.out.println(1);
+            Repository repository = repositoryService.getRepository(repositoryId);
+            System.out.println(1);
+
+            System.out.println(repository.getDescription());
+            System.out.println(repository.getGitUrl());
+            // Create the pull request
+            pullRequestService.createPullRequest(repository, pullRequest);
+
+            System.out.println("Pull request created successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println(7);
-        return "success";
 
+        return "success";
     }
 }
