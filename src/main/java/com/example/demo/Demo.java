6ee52c593d64e3f80c9b95982cdd0e2ad77a03c2
Sisuka Udawatta <works.sisuka@gmail.com>
2023-06-15 12:47:35 +0530

    1

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
-import org.eclipse.egit.github.core.PullRequestMarker;
-import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.*;
 import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.IssueService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.eclipse.egit.github.core.PullRequest;
-import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.service.PullRequestService;
 import org.eclipse.egit.github.core.service.RepositoryService;
 
@@ -21,18 +19,18 @@ import java.io.IOException;
 public class Demo {
 
     @GetMapping
-    public String get(){
+    public PullRequest get() throws IOException {
         return pullReq();
     }
 
-    public String pullReq() {
+    public PullRequest pullReq() throws IOException {
 
         String username = "mr-sizka";
         String password = "Sisuka#9ruuqpjpy";
         String repositoryOwner = "Mr-Sizka";
         String repositoryName = "demo";
 
-        GitHubClient client = new GitHubClient();
+        /*GitHubClient client = new GitHubClient();
         client.setCredentials(username, password);
 
 
@@ -43,22 +41,16 @@ public class Demo {
             pullRequest.setTitle("My Pull Request");
             pullRequest.setBody("This is a pull request from the Java application.");
 
-            System.out.println(1);
             // Specify the source and destination branches
-            pullRequest.setBase(new PullRequestMarker().setRef("main").setLabel("main"));
-            pullRequest.setHead(new PullRequestMarker().setRef("feet3").setLabel("feet3"));
+            pullRequest.setBase(new PullRequestMarker().setRef("feet3").setLabel("feet3"));
+            pullRequest.setHead(new PullRequestMarker().setRef("main").setLabel("main"));
 
 
             // Get the repository information
             RepositoryId repositoryId = RepositoryId.create(repositoryOwner, repositoryName);
-            System.out.println(1);
             RepositoryService repositoryService = new RepositoryService(client);
-            System.out.println(1);
-            Repository repository = repositoryService.getRepository(repositoryId);
-            System.out.println(1);
+            Repository repository = repositoryService.getRepository("Mr-Sizka","demo");
 
-            System.out.println(repository.getDescription());
-            System.out.println(repository.getGitUrl());
             // Create the pull request
             pullRequestService.createPullRequest(repository, pullRequest);
 
@@ -67,6 +59,46 @@ public class Demo {
             e.printStackTrace();
         }
 
-        return "success";
+        return "success";*/
+
+        /*RepositoryService repositoryService = new RepositoryService();
+        repositoryService.getClient().setCredentials(username, password);
+        org.eclipse.egit.github.core.Repository openmrs = null;
+        try {
+            openmrs = repositoryService.getRepository(repositoryOwner, repositoryName);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to get repository data", e);
+        }
+        PullRequestService pullRequestService = new PullRequestService();
+        pullRequestService.getClient().setCredentials(username, password);
+        PullRequest pr = new org.eclipse.egit.github.core.PullRequest();
+        pr.setTitle("pull request method");
+        pr.setHead(new PullRequestMarker().setLabel("feet3"));
+        pr.setBase(new PullRequestMarker().setLabel("main"));
+        pr.setBody("request.getDescription()");
+
+        try {
+            return pullRequestService.createPullRequest(openmrs, 3,"feet3","main");
+        } catch (IOException e) {
+            System.out.println("failed");
+            throw new RuntimeException("Failed to create pull request:"+pr, e);
+        }*/
+
+
+        GitHubClient client = new GitHubClient();
+        client.setCredentials(username, password);
+
+        PullRequestService pullRequestService = new PullRequestService(client);
+        pullRequestService.getClient().setCredentials(username, password);
+        RepositoryId repo = RepositoryId.createFromId(repositoryOwner + "/" + repositoryName);
+
+        PullRequest request = new PullRequest();
+        request.setTitle("Please merge");
+        request.setBody("Please merge");
+        request.setHead(new PullRequestMarker().setRef("feet3").setLabel("feet3"));
+        request.setBase(new PullRequestMarker().setRef("master").setLabel("master"));
+
+        return pullRequestService.createPullRequest(repo,request);
     }
+
 }
